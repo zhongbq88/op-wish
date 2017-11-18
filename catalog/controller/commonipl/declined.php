9b9c3aaba6f03bb27eeda7bc9faef9a9a0e3a73d
zhongbq88 <zhongbq88@gmail.com>
2017-11-18 19:40:18 +0800

    update

--- catalog/controller/commonipl/declined.php
+++ catalog/controller/commonipl/declined.php
@@ -3,13 +3,11 @@ class ControllerCommoniplDeclined extends Controller {
 	public function index() {
 		
 
-			$charging = $this->config->get('config_charging');
-
+			$data['error_warning'] = true;
 			
-
 			if(isset($this->session->data['shop'])){
 				$data['shopify'] = $this->session->data['shop'];
-				$data['error_warning'] = html_entity_decode(sprintf($charging['tips'], $this->url->link('shopify/connect', ''), 'https://'.$data['shopify'].'/admin/apps'), ENT_QUOTES, 'UTF-8');
+				
 			}
 		
 			$data['appkey'] = SHOPIFY_APP_API_KEY;
