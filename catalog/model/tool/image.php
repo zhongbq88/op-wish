4346af348b2eed3ce779e3e3e833d6b570200302
kylerhen <zqchen318@gmail.com>
2017-11-25 21:56:02 +0800

    update

--- catalog/model/tool/image.php
+++ catalog/model/tool/image.php
@@ -11,7 +11,15 @@ class ModelToolImage extends Model {
 		$image_new = 'cache/' . utf8_substr($filename, 0, utf8_strrpos($filename, '.')) . '-' . (int)$width . 'x' . (int)$height . '.' . $extension;
 
 		if (!is_file(DIR_IMAGE . $image_new) || (filemtime(DIR_IMAGE . $image_old) > filemtime(DIR_IMAGE . $image_new))) {
-			list($width_orig, $height_orig, $image_type) = getimagesize(DIR_IMAGE . $image_old);
+			try
+			{
+				list($width_orig, $height_orig, $image_type) = getimagesize(DIR_IMAGE . $image_old);
+			}
+			catch (Exception $e)
+			{
+				return $filename;
+			}
+			
 				 
 			if (!in_array($image_type, array(IMAGETYPE_PNG, IMAGETYPE_JPEG, IMAGETYPE_GIF))) { 
 				return DIR_IMAGE . $image_old;
