6c03e4b8ae0163058150040f05edeeedd23f1361
zbq <zhongbq88@gmail.com>
2017-12-03 10:02:06 +0000

    update

--- catalog/controller/commonipl/category.php
+++ catalog/controller/commonipl/category.php
@@ -32,7 +32,7 @@ class ControllerCommoniplCategory extends Controller {
 				$data['categories'][] = array(
 					'name' => $result['name'],
 					/*'thumb' =>$image,*/
-					'productlist' => $this->load($result['category_id'])
+					'productlist' => $this->load->controller('commonipl/products/load',$result['category_id'])
 				);
 			}
 		}
@@ -61,70 +61,4 @@ class ControllerCommoniplCategory extends Controller {
 		$this->response->setOutput($this->load->view('commonipl/category', $data));
 	}
 	
-	public function load($category_id) {
-	
-		$this->load->model('commonipl/product');
-		$data['products'] = array();
-		$this->load->model('account/wishlist');
-		$wishlistProductId = $this->model_account_wishlist->getWishlistProductId();
-		$results = $this->model_commonipl_product->getProductsByCategoryId($category_id);
-		foreach ($results as $result) {
-			if ($result['image']) {
-				$image = $this->model_tool_image->resize($result['image'], $this->config->get('theme_' . $this->config->get('config_theme') . '_image_product_width'), $this->config->get('theme_' . $this->config->get('config_theme') . '_image_product_height'));
-			} else {
-				$image = $this->model_tool_image->resize('placeholder.png', $this->config->get('theme_' . $this->config->get('config_theme') . '_image_product_width'), $this->config->get('theme_' . $this->config->get('config_theme') . '_image_product_height'));
-			}
-
-			if ($this->customer->isLogged() || !$this->config->get('config_customer_price')) {
-				$price = $this->currency->format($this->tax->calculate($result['price'], $result['tax_class_id'], $this->config->get('config_tax')), $this->session->data['currency']);
-			} else {
-				$price = false;
-			}
-
-			if (isset($result['special'])&&(float)$result['special']) {
-				$special = $this->currency->format($this->tax->calculate($result['special'], $result['tax_class_id'], $this->config->get('config_tax')), $this->session->data['currency']);
-			} else {
-				$special = false;
-			}
-
-			if ($this->config->get('config_tax')) {
-				$tax = $this->currency->format(isset($result['special'])&&(float)$result['special'] ? $result['special'] : $result['price'], $this->session->data['currency']);
-			} else {
-				$tax = false;
-			}
-
-			if ($this->config->get('config_review_status')&&isset($result['rating'])) {
-				$rating = (int)$result['rating'];
-			} else {
-				$rating = false;
-			}
-
-			$data['products'][] = array(
-				'product_id'  => $result['product_id'],
-				'thumb'       => $image,
-				'name'        => utf8_substr(trim(strip_tags(html_entity_decode($result['name'], ENT_QUOTES, 'UTF-8'))), 0, 60) . '..',
-				'description' => utf8_substr(trim(strip_tags(html_entity_decode($result['description'], ENT_QUOTES, 'UTF-8'))), 0, $this->config->get('theme_' . $this->config->get('config_theme') . '_product_description_length')) . '..',
-				'price'       => $price,
-				'special'     => $special,
-				'tax'         => $tax,
-				'minimum'     => $result['minimum'] > 0 ? $result['minimum'] : 1,
-				'rating'      => isset($result['rating'])?$result['rating']:0,
-				'added'       => $this->inArray($result['product_id'],$wishlistProductId),
-				'href'        => $this->url->link('commonipl/product','product_id=' . $result['product_id'])
-			);
-		}
-		return $this->load->view('commonipl/products', $data);	
-
-	}
-	
-	private function inArray($product_id,$array){
-		
-		foreach($array as $value){
-			if($product_id == $value['product_id']){
-				
-				return 1;
-			}
-		}
-		return 0;
-	}
 }
--- catalog/controller/commonipl/products.php
+++ catalog/controller/commonipl/products.php
@@ -82,6 +82,64 @@ class ControllerCommoniplProducts extends Controller {
 
 	}
 	
+	public function load($category_id) {
+	
+		$this->load->model('commonipl/product');
+		$data['products'] = array();
+		$this->load->model('account/wishlist');
+		$coupons =  $this->model_commonipl_product->getCoupons();
+		$charging = $this->config->get('config_charging');
+		$wishlistProductId = $this->model_account_wishlist->getWishlistProductId();
+		$results = $this->model_commonipl_product->getProductsByCategoryId($category_id);
+		foreach ($results as $result) {
+			if ($result['image']) {
+				$image = $this->model_tool_image->resize($result['image'], $this->config->get('theme_' . $this->config->get('config_theme') . '_image_product_width'), $this->config->get('theme_' . $this->config->get('config_theme') . '_image_product_height'));
+			} else {
+				$image = $this->model_tool_image->resize('placeholder.png', $this->config->get('theme_' . $this->config->get('config_theme') . '_image_product_width'), $this->config->get('theme_' . $this->config->get('config_theme') . '_image_product_height'));
+			}
+
+			$ninMaxPrice = $this->model_commonipl_product->getMinMaxPrice($result['product_id']);
+			
+			$price = $this->getPriceFormat($result,0,$ninMaxPrice);
+
+			if (isset($result['special'])&&(float)$result['special']) {
+				$special = $this->currency->format($this->tax->calculate($result['special'], $result['tax_class_id'], $this->config->get('config_tax')), $this->session->data['currency']);
+			} else {
+				$special = false;
+			}
+
+			if ($this->config->get('config_tax')) {
+				$tax = $this->currency->format(isset($result['special'])&&(float)$result['special'] ? $result['special'] : $result['price'], $this->session->data['currency']);
+			} else {
+				$tax = false;
+			}
+
+			if ($this->config->get('config_review_status')&&isset($result['rating'])) {
+				$rating = (int)$result['rating'];
+			} else {
+				$rating = false;
+			}
+
+			$data['products'][] = array(
+				'product_id'  => $result['product_id'],
+				'thumb'       => $image,
+				'name'        => utf8_substr(trim(strip_tags(html_entity_decode($result['name'], ENT_QUOTES, 'UTF-8'))), 0, 60) . '..',
+				'description' => utf8_substr(trim(strip_tags(html_entity_decode($result['description'], ENT_QUOTES, 'UTF-8'))), 0, $this->config->get('theme_' . $this->config->get('config_theme') . '_product_description_length')) . '..',
+				'price'       => $price,
+				'special'     => $special,
+				'tax'         => $tax,
+				'coupon'      => $this->getCoupon($coupons,$category_id,$result,$ninMaxPrice),
+				'vipdiscount' => $charging['open'],
+				'minimum'     => $result['minimum'] > 0 ? $result['minimum'] : 1,
+				'rating'      => isset($result['rating'])?$result['rating']:0,
+				'added'       => $this->inArray($result['product_id'],$wishlistProductId),
+				'href'        => $this->url->link('commonipl/product','product_id=' . $result['product_id'])
+			);
+		}
+		return $this->load->view('commonipl/products', $data);	
+
+	}
+	
 	
 	private function getPriceFormat($result,$discount,$ninMaxPrice){
 			
